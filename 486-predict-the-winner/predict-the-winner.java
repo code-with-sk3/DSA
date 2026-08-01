d3937ce392a9af077ae9623d82144a83a75952eb
Shivan kaushik <sk9548265968@gmail.com>
2026-08-01 10:14:32 -0700

    Time: 66 ms (23.33%) | Memory: 42.3 MB (97.59%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        return solve(0, nums.length - 1, nums) >= 0;
+    }
+
+    private int solve(int i, int j, int[] nums) {
+        if (i == j)
+            return nums[i];
+
+        int left = nums[i] - solve(i + 1, j, nums);
+        int right = nums[j] - solve(i, j - 1, nums);
+
+        return Math.max(left, right);
+    }
+}
