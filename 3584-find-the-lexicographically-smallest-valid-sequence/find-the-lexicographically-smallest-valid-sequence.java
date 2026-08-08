56bf91402953f8e09ddb252ae74bbb81282299e9
Shivan kaushik <sk9548265968@gmail.com>
2026-08-08 08:36:18 -0700

    Time: 31 ms (97.22%) | Memory: 137.7 MB (22.22%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[] ans = new int[m];
+
+        // last[j] = last possible index in word1
+        // where word2[j] can be matched
+        int[] last = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            last[i] = -1;
+        }
+
+        // Find positions from right to left
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (word1.charAt(i) == word2.charAt(j)) {
+                last[j] = i;
+                j--;
+            }
+            i--;
+        }
+
+        // If word2 itself cannot be formed as a subsequence
+        // even after considering one mismatch, we handle it greedily below.
+
+        boolean mismatchUsed = false;
+        j = 0;
+
+        for (i = 0; i < n && j < m; i++) {
+
+            // Exact match
+            if (word1.charAt(i) == word2.charAt(j)) {
+                ans[j] = i;
+                j++;
+            }
+
+            // Use our one allowed mismatch
+            else if (!mismatchUsed &&
+                    (j == m - 1 || i < last[j + 1])) {
+
+                ans[j] = i;
+                j++;
+                mismatchUsed = true;
+            }
+        }
+
+        // Could not create a valid sequence
+        if (j != m) {
+            return new int[0];
+        }
+
+        return ans;
+    }
+}
